61abcd9bc858eedda5f1e2745e19f9168fb572bd
Szymon Mackiewicz <szymon.mackiewicz63@gmail.com>
2024-07-23 11:47:29 +0200

    refactor: change hero css to module

--- client/src/components/Hero/Hero.tsx
+++ client/src/components/Hero/Hero.tsx
@@ -1,14 +1,14 @@
-import "./Hero.css"
+import styles from "./Hero.module.css"
 
 import image from "../../assets/hero.jpg"
 
 function Hero() {
   return (
-    <section className="hero">
-        <h1 className="hero__header">SALE</h1>
-        <h2 className="hero__subheader">up to 70% off</h2>
+    <section className={styles.hero}>
+        <h1 className={styles.header}>SALE</h1>
+        <h2 className={styles.subheader}>up to 70% off</h2>
         <button className="btn btn--primary">Buy Now</button>
-        <img className="hero__img" src={image} alt="" />
+        <img className={styles.img} src={image} alt="" />
     </section>
   )
 }
