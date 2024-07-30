be74d56f0055d7071434a2abffd043958426eb34
Szymon Mackiewicz <szymon.mackiewicz63@gmail.com>
2024-07-30 09:17:25 +0200

    feat: add newsletter component

--- client/src/components/Newsletter/Newsletter.tsx
+++ client/src/components/Newsletter/Newsletter.tsx
@@ -4,7 +4,14 @@ type Props = {}
 
 function Newsletter({}: Props) {
   return (
-    <section className={`${styles.main} container`}>Newsletter</section>
+    <section className={styles.main}>
+      <h5 className={styles.title}>Sign up for the newsletter and get 10% off</h5>
+      <p className={styles.subtitle}>Receive the latest news and trends each week.</p>
+      <form className={styles.form}>
+        <input className={styles.input} type="email" required placeholder="e-mail" />
+        <button className="btn btn--tertiary">Sign Up</button>
+      </form>
+    </section>
   )
 }
 
